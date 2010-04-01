28b901e06c0e44c559e137ec471c84a0168fa073
gerich-home <gerich.home@gmail.com>
2010-04-01 17:00:01 +0000

    Создана исходная структура хранилища. Создан тестовый проект. close #1

---
+++ EvoMice/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvoMice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EvoMice");
+            Console.ReadLine();
+        }
+    }
+}
